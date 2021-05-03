f9ceecd727fb21fb324f56c1e8a98a62478a4ed7
Mohammd1321 <69431624+Mohammd1321@users.noreply.github.com>
2021-05-03 17:57:32 +0300

    clock-files

--- clock-app/clock.js
+++ clock-app/clock.js
@@ -1,21 +1,37 @@
 'use strict';
 
-const clock = document.querySelector('.middle');
+const clock = document.querySelector('.timer');
+const party = document.querySelector('.party');
+const video = document.querySelector('video');
+
+
+let date = new Date(2000,1,1,12,0,0)
 
 function showTime() {
   let hours = new Date().getHours();
   let minutes = new Date().getMinutes();
   let seconds = new Date().getSeconds();
   let duration = 'am';
+  let end = video.ended;
   
   if(hours == 0) {
     hours = 12;
   }
 
-
   if(hours > 12) {
     hours = hours - 12;
     duration = 'pm'
+  }
+
+  if(hours == 12 && minutes == 0 && seconds == 0) {
+    party.setAttribute('id','awesome');
+    display(true);
+    video.load();
+  }
+
+  if(end) {
+    party.removeAttribute('id');
+    display(false);
   }
 
   hours = hours < 10 ? '0' + hours: hours;
@@ -31,4 +47,7 @@ let timer = setInterval(showTime,1000);
 showTime();
 
 
-
+function display(value = false) {
+  video.controls = value;
+  video.autoplay = value;
+}
